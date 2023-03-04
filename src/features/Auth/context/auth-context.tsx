17bad991e61c13f96071a5bd423b2c481e2d0b49
medreres <90519417+medreres@users.noreply.github.com>
2023-03-04 22:10:18 +0200

    authorizaion

---
+++ src/features/Auth/context/auth-context.tsx
@@ -0,0 +1,75 @@
+import React, { createContext, useContext, useEffect } from "react";
+
+import useLocalStorage from "../../../hooks/useLocalStorage";
+import { getDateUnix } from "../../../utils/format";
+
+interface AuthContextProviderProps {
+  children: React.ReactNode;
+}
+
+interface AuthContextValue {
+  idToken: string | null;
+  setIdToken: (user: string | null) => void;
+  profile: Profile | null;
+  setProfile: (profile: Profile | null) => void;
+}
+
+interface Profile {
+  email: string;
+  exp: number;
+  family_name: string;
+  given_name: string;
+  iat: number;
+  name: string;
+  picture: string;
+}
+
+const AuthContext = createContext<AuthContextValue | null>(null);
+
+export const useAuthContext = () => useContext(AuthContext) as AuthContextValue;
+
+const AuthContextProvider = ({ children }: AuthContextProviderProps) => {
+  const [idToken, setIdToken] = useLocalStorage<string | null>("INTH20_ID_TOKEN", null);
+  const [profile, setProfile] = useLocalStorage<Profile | null>("INTH20_PROFILE_DECODED", null);
+
+  useEffect(() => {
+    // console.log(jwtDecode(idToken))
+    // if token is null - exit
+    // console.log(idToken);
+    if (idToken == null) return;
+
+    // if token is present, but not valid - set to null
+    if (getDateUnix(profile!.exp) < new Date()) {
+      setIdToken(null);
+      setProfile(null);
+      return;
+    }
+  }, [idToken, profile, setIdToken, setProfile]);
+
+  useEffect(() => {
+    if (!profile) return;
+
+    const time = getDateUnix(profile!.exp).getMilliseconds() - new Date().getMilliseconds();
+    const clearProfile = () => {
+      setIdToken(null);
+      setProfile(null);
+    };
+    const cleanup = setTimeout(clearProfile, time);
+
+    return () => clearTimeout(cleanup);
+  }, [profile]);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        idToken,
+        setIdToken,
+        profile,
+        setProfile,
+      }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContextProvider;
