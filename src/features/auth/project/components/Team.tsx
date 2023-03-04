7f13e7b15e728b7329b1d1a5050b70c497786b49
medreres <90519417+medreres@users.noreply.github.com>
2023-03-05 01:00:36 +0200

    container with requests

---
+++ src/features/auth/project/components/Team.tsx
@@ -0,0 +1,39 @@
+import { Typography } from "@mui/material";
+import { Box, Stack } from "@mui/system";
+import React from "react";
+import AccountCircleTwoToneIcon from "@mui/icons-material/AccountCircleTwoTone";
+
+interface ITeam {
+  members: any[];
+}
+export default function Team({ members }: ITeam) {
+  const arr = [1, 2, 3];
+  const status = "OPEN";
+  return (
+    <Stack alignItems={"flex-start"}>
+      <Typography
+        variant="caption"
+        sx={{
+          textTransform: "uppercase",
+          fontSize: 15,
+          color: "#8B949E",
+        }}
+        fontWeight={300}>
+        Team
+      </Typography>
+      <Box display={"flex"}>
+        {arr.map((member, index) => (
+          <img
+            height={64}
+            src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"
+            style={{
+              transform: `translate(${-40 * index}%)`,
+              zIndex: index,
+              borderRadius: "100%",
+            }}
+          />
+        ))}
+      </Box>
+    </Stack>
+  );
+}
--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -1,13 +1,105 @@
+import { Box, Button, Chip, Divider, Grid, Typography } from "@mui/material";
+import { Container, Stack } from "@mui/system";
 import React from "react";
 import { useParams } from "react-router-dom";
 import BackButton from "../components/BackButton";
+import Request from "../features/auth/project/components/Request";
+import Team from "../features/auth/project/components/Team";
 
 export default function Project() {
   const { projectId } = useParams();
+  const test = [1, 2, 3, 4];
   console.log(projectId);
   return (
-    <>
-      <BackButton />
-    </>
+    <Grid
+      container
+      px={{ xs: "36px", sm: "48px", md: "96px" }}
+      py={{ xs: "48px" }}
+      display="flex"
+      flexDirection={"column"}
+      flex="1"
+      justifyContent="space-between"
+      gap={2}
+      // sx={{
+      //   backgroundColor: "rgba(0, 58, 250, 0.1)",
+      // }}
+    >
+      <Box
+        display={"flex"}
+        justifyContent={"flex-start"}
+        marginLeft={"-12px"}>
+        <BackButton />
+      </Box>
+      <img
+        // width={1072}
+        height={250}
+        src="https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"
+        alt="placeholder"
+      />
+      <Box
+        display={"flex"}
+        justifyContent="space-between"
+        alignItems="center">
+        <Stack
+          gap={1}
+          direction={"row"}>
+          <Chip
+            sx={{
+              color: "#8B949E",
+            }}
+            label="#tag"
+            variant="outlined"
+          />
+          <Chip
+            sx={{
+              color: "#8B949E",
+            }}
+            label="#tag"
+            variant="outlined"
+          />
+        </Stack>
+        <Button
+          variant="contained"
+          sx={{
+            color: "#8B949E",
+            backgroundColor: "#D9D9D9",
+            borderRadius: 5,
+            textTransform: "lowercase",
+          }}>
+          open
+        </Button>
+      </Box>
+      <Stack
+        alignItems={"flex-start"}
+        gap={2}
+        marginBottom={2}>
+        <Typography variant="h1">Project Name</Typography>
+        <Typography
+          variant="body1"
+          textAlign={"start"}>
+          Lorem ipsum dolor sit amet consectetur. At viverra mattis dui nascetur. Neque massa scelerisque euismod
+          pulvinar erat duis non leo et. Bibendum non sagittis laoreet vitae integer quis eu convallis. Sapien rhoncus
+          fringilla pulvinar consectetur.
+        </Typography>
+        <Team members={[]} />
+        <Button
+          variant="contained"
+          sx={{
+            color: "#8B949E",
+            backgroundColor: "#D9D9D9",
+            borderRadius: 5,
+            textTransform: "none",
+            px: 5,
+          }}>
+          Start Project
+        </Button>
+      </Stack>
+      <Divider />
+      <Stack gap={2}>
+        {test.map((member, index) => (
+          <Request key={index} />
+        ))}
+      </Stack>
+    </Grid>
   );
 }
