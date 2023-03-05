0e51f780f440141ea24754a890699698dc2a06be
medreres <90519417+medreres@users.noreply.github.com>
2023-03-05 06:12:37 +0200

    main serach page

--- src/pages/DiscoverProjects.tsx
+++ src/pages/DiscoverProjects.tsx
@@ -1,12 +1,33 @@
-import React, { useState } from 'react';
-import { render } from 'react-dom';
-import { WithContext as ReactTags } from 'react-tag-input';
-
+import { CircularProgress, TextField, Typography } from "@mui/material";
+import { Stack } from "@mui/system";
+import React, { useEffect, useState } from "react";
+import { render } from "react-dom";
+import { WithContext as ReactTags } from "react-tag-input";
+import useProjects from "../hooks/useProjects";
 
 const DiscoverProjects = () => {
-  return (
-    <></>
-  )
-}
+  const [projects, isPending, search] = useProjects();
+  useEffect(() => {
+    search([]);
+  }, []);
 
-export default DiscoverProjects
+  return (
+    <>
+      <TextField label="Tags" />
+      {isPending && <CircularProgress color="secondary" />}
+      <Stack>
+        {!isPending && (
+          <>
+            {projects.map((project) => (
+              <div key={project.id}>
+                <Typography color={"secondary"}>{project.title}</Typography>
+              </div>
+            ))}
+          </>
+        )}
+      </Stack>
+    </>
+  );
+};
+
+export default DiscoverProjects;
